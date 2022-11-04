8b6919d2755a78b3957b53a76cbf2f1dc9e7d366
bowmanchow <bowmanchow@gmail.com>
2022-11-04 21:14:45 +0800

    add cases to config

--- config.py
+++ config.py
@@ -5,6 +5,9 @@ METHOD_NAME = "base"
 MAX_EPOCH = 150
 TRAIN_BATCH_PER_GPU = 34
 VALID_BATCH_PER_GPU = 36
-GPU_S = "5"
+GPU_S = "0"
 LEARNING_RATE = 1e-4
 BASE_MODEL_PATH = None
+
+ALL_CASES = list(range(160)) + list(range(161, 300))
+TRAINING_CASES = list(range(160)) + list(range(161, 210))
